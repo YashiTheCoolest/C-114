f737b1100030aaa4bfcb064a4cb70a54c2490509
YashiTheCoolest <y4shi5hashwat@gmail.com>
2024-06-10 19:01:57 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,25 @@ function preload(){
 function setup(){
   canvas = createCanvas(300,300)
   canvas.position(220,200)
+  video = createCapture(VIDEO)
+  video.hide()
+  video.size(300,300)
+  poseNet = ml5.poseNet(video, modelLoaded)
+  poseNet.on('pose', gotPoses)
+}
+function modelLoaded(){
+  console.log('PoseNet is Initialized')
+}
+function gotPoses(results){
+  if(results.length > 0)
+    {
+      console.log(results)
+      console.log("nose x = " + results[0].pose.nose.x)
+      console.log("nose y = " + results[0].pose.nose.y)
+    }
 }
 function draw(){
-  
+  image(video, 0, 0, 300, 300)
 }
 function click_pic(){
   save("funnyFacez.png")
