4208f6b0d755e599c87c1750d11f9bde50488c85
YashiTheCoolest <y4shi5hashwat@gmail.com>
2024-06-10 17:56:51 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+  
+}
+function setup(){
+  canvas = createCanvas(300,300)
+  canvas.position(220,200)
+}
+function draw(){
+  
+}
+function click_pic(){
+  save("funnyFacez.png")
+}
